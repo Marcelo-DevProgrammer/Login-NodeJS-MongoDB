ed3fd6c36fec0af06ca773bbffe79f8aa4fb3e11
Isaquedevhub <isaque.freires1998@hotmail.com>
2025-11-13 15:21:18 -0300

    fix: Security arranged.

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
   try {
-    await mongoose.connect("mongodb+srv://madevhub:Mappe20233@backendcampeonato.oaonq.mongodb.net/LoginSistem?retryWrites=true&w=majority&appName=LoginSistem", {
+    await mongoose.connect(process.env.MONGO_URL, {
       serverSelectionTimeoutMS: 30000, // aumenta o tempo limite pra 30 segundos
       socketTimeoutMS: 45000,          // aumenta o tempo do socket
       bufferCommands: false,           // desativa o buffer de comandos
